a2e2376f10e4b597702e1360b581876d18d2ab4c
Audrey Lee <audrey06lee05@gmail.com>
2026-08-17 13:09:02 +0100

    Add readJson/writeJson storage helpers

---
+++ server/src/storage.ts
@@ -0,0 +1,26 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+function dataPath(filename: string): string {
+  return path.join(__dirname, "..", "data", filename);
+}
+
+export async function readJson<T>(filename: string): Promise<T> {
+  try {
+    const filePath = dataPath(filename);
+    const data = await fs.readFile(filePath, "utf-8");
+
+    return JSON.parse(data) as T;
+  } catch (err: any) {
+    if (err.code === "ENOENT") {
+      return [] as unknown as T;
+    }
+    throw err;
+  }
+}
+
+export async function writeJson<T>(filename: string, data: T): Promise<void> {
+  const filePath = dataPath(filename);
+  const json = JSON.stringify(data, null, 2);
+  await fs.writeFile(filePath, json, "utf-8");
+}
